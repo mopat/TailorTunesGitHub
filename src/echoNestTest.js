3fec8b8a83d04b930e2a6d2e7761284e4af6473e
mopat <morczinietz@gmail.com>
2014-10-27 21:12:02 +0100

    Test 2

--- src/echoNestTest.js
+++ src/echoNestTest.js
@@ -1,14 +1,57 @@
 /**
  * Created by Patrick on 27.10.2014.
  */
+var echoNestKey = "N2U2OZ8ZDCXNV9DBG";
 $.ajax({
     type: "GET",
-    url: "http://developer.echonest.com/api/v4/song/search?api_key=N2U2OZ8ZDCXNV9DBG&sort=song_hotttnesss-desc&bucket=song_hotttnesss",
+    url: "http://developer.echonest.com/api/v4/playlist/static?api_key=N2U2OZ8ZDCXNV9DBG&format=json&artist=snoop+dogg&artist_start_year_after=1991&artist_start_year_before=2000&sort=song_hotttnesss-desc&results=80",
     cache: false,
-    success: function(datas){
-        console.log(datas);
+    success: function(jsonObject){
+        console.log(jsonObject);
+        console.log(jsonObject.response.songs);
     },
     error: function(XMLHttpRequest, textStatus, errorThrown) {
-        alert("Error " + errorThrown + " at" + XMLHttpRequest);
+        alert("ERROR " + errorThrown + " at" + XMLHttpRequest);
+    }
+});
+
+//echonest artist + hotttnes
+$("#button-start-search").on("click", function(){
+    console.log("send");
+    var requestText = $("#input-song-search").val();
+    console.log(requestText);
+    $.ajax({
+        type: "GET",
+        url: "http://developer.echonest.com/api/v4/playlist/static?api_key="+echoNestKey+"&format=json&artist="+requestText+"&sort=song_hotttnesss-desc&bucket=song_currency&results=80",
+        cache: false,
+        success: function(jsonObject){
+            //console.log(jsonObject);
+            console.log(jsonObject.response.songs[0].title);
+            var songs = jsonObject.response.songs;
+            for(var i = 0 ; i < songs.length; i++){
+                console.log(songs[i].title);
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("ERROR " + errorThrown + " at" + XMLHttpRequest);
+        }
+    });
+});
+
+//Spotify Requests artist + year
+$.ajax({
+    type: "GET",
+    url: "https://api.spotify.com/v1/search?q=snoop+dogg+year:2005-2014&type=track",
+    cache: false,
+    success: function(data){
+        console.log(data);
+        var tracks = data.tracks.items;
+        for(var i = 0 ; i < tracks.length; i++){
+            console.log(tracks[i].name);
+        }
+
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+        alert("ERROR " + errorThrown + " at" + XMLHttpRequest);
     }
 });
