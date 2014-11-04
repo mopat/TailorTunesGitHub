b3d2cffa81adae70aad9dad49bcc8c19bfab7a52
mopat <morczinietz@gmail.com>
2014-11-04 23:42:12 +0100

    info.js added +  some mods

--- responsive/responsivesrc/info.js
+++ responsive/responsivesrc/info.js
@@ -3,13 +3,21 @@
  */
 var $infoHeader = $("#info-header");
 var $infoContent = $("#info-content");
+var $document = $(document);
+var $documentWidth = $(document).width();
 
 $infoHeader.on("click", function(e){
     e.preventDefault();
-    if($infoContent.is(":hidden")){
-        $infoContent.show(100);
-    }
-    else{
-        $infoContent.hide(100);
+        if($infoContent.is(":hidden")){
+            $infoContent.show(100);
+        }
+        else{
+            $infoContent.hide(100);
+        }
+});
+$(window).resize(function(){
+    console.log($document.width());
+    if($documentWidth > 1024){
+        $infoContent.show();
     }
 });
