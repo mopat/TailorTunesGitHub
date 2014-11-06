af2184534d34196ac6b107d97ef43f819f6d55ce
mopat <morczinietz@gmail.com>
2014-11-06 13:37:32 +0100

    sidebar desktop workin

--- responsive/responsivesrc/info.js
+++ responsive/responsivesrc/info.js
@@ -4,20 +4,20 @@
 var $infoIcon = $("#info-icon");
 var $infoContent = $("#info-content");
 
-$infoIcon.on("click", function(e){
+$infoIcon.on("click", function (e) {
     e.preventDefault();
-        if($infoContent.is(":hidden")){
-            $infoContent.show(100);
-        }
-        else{
-            $infoContent.hide(100);
-        }
+    if ($infoContent.is(":hidden")) {
+        $infoContent.show(100);
+    }
+    else {
+        $infoContent.hide(100);
+    }
 });
-$(window).resize(function(){
-    if($(window).width() > 1024){
+$(window).resize(function () {
+    if ($(window).width() > 1024) {
         $infoContent.show();
     }
-    else{
+    else {
         $infoContent.hide();
     }
 });
