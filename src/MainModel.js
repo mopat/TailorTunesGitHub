8511f8e17d70f508aadb8a964bd87a71e2625243
mopat <morczinietz@gmail.com>
2014-12-02 16:18:24 +0100

    mutliple ajax requests

--- src/MainModel.js
+++ src/MainModel.js
@@ -4,6 +4,7 @@ App.MainModel = (function () {
         client_id = '23a3031c7cd251c7c217ca127777e48b',
         limit = 200,
         properTracks = [],
+        stringScoreTolerance = 0.5,
 
         init = function () {
             console.log("MM");
@@ -12,7 +13,7 @@ App.MainModel = (function () {
         },
 
         initSoundCloud = function () {
-// initialize client with app credentials
+            // initialize client with app credentials
             SC.initialize({
                 client_id: client_id
             });
@@ -26,10 +27,10 @@ App.MainModel = (function () {
                 url: "https://developer.echonest.com/api/v4/playlist/static?api_key=N2U2OZ8ZDCXNV9DBG&format=json&artist=snoop+dogg&artist_start_year_after=1991&artist_start_year_before=2000&sort=song_hotttnesss-desc&results=80",
                 cache: false,
                 success: function (jsonObject) {
-                    spotifyArtist = jsonObject.response.songs[0].artist_name;
-                    spotifyTitle = jsonObject.response.songs[0].title;
+                    spotifyArtist = normalize(jsonObject.response.songs[0].artist_name);
+                    spotifyTitle = normalize(jsonObject.response.songs[0].title);
                     //console.log(jsonObject.response.songs);
-                    searchSoundCloudTracks("nicky minaj", "anaconda");
+                    searchSoundCloudTracks("rihanna", "the monster");
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     alert("ERROR " + errorThrown + " at" + XMLHttpRequest);
@@ -40,10 +41,11 @@ App.MainModel = (function () {
         searchSoundCloudTracks = function (artist, title) {
             var queryOne = artist + " " + title;
             var queryTwo = title + " " + artist;
-            var queryThree = title;
+            var queryThree = title + " official";
+            var testString = normalize("Eminem - The Monster Feat. Rihanna (Official Audio)");
+            var testScore = testString.score(queryTwo);
+            console.log("TESTSCORE " + testScore);
 
-            var bestResult = null;
-            var tracks = [];
             $.ajax({
                 url: getAjaxUrl(queryOne),
                 data: {
@@ -81,18 +83,32 @@ App.MainModel = (function () {
                 },
                 dataType: 'json',
                 success: function (data) {
-                    deleteNotMatchingResults(data, queryThree);
-                    properTracks.sort(sortByPlaybackCount);
-
-                    bestResult = properTracks[0];
-                    console.log(bestResult.title);
-                    console.log(bestResult.playback_count);
-                    iterateArray(properTracks);
-                    playTrack(bestResult.stream_url);
-
+                    deleteNotMatchingResults(data, queryOne);
+                    playTrack(getBestTrack().stream_url);
                 },
                 type: 'GET'
             });
+        },
+
+        getBestTrack = function () {
+            var tracks = [];
+            //sort tracks by score
+            tracks.sort(sortByScore);
+            //take tracks with best match
+            for (var i = 0; i < 20; i++) {
+                tracks[i] = properTracks[i];
+                iterateArray(tracks);
+            }
+
+            //sort tracks by playback_count
+            tracks.sort(sortByFavoritingsCount);
+
+            console.log(tracks[0].title);
+            console.log("playbackcount " + tracks[0].playback_count);
+            console.log(tracks[0].score);
+            //take the first element
+            return tracks[0];
+
         },
 
         sortByFavoritingsCount = function (a, b) {
@@ -103,43 +119,31 @@ App.MainModel = (function () {
             return b.playback_count - a.playback_count;
         },
 
-        getBestScoreAndCountTrack = function (tracks) {
-            var bestTrack = null;
-            for (var i = 0; i < tracks.length - 1; i++) {
-                var currentTrack = tracks[i];
-                var nextTrack = tracks[i + 1];
-                if (currentTrack.favoritings_count > nextTrack.favoritings_count) {
-                    bestTrack = currentTrack;
-                }
-                else {
-                    bestTrack = nextTrack;
-                }
-            }
-            console.log(bestTrack.favoritings_count);
-            return bestTrack;
-        },
-
         deleteNotMatchingResults = function (tracks, query) {
+            var count = 0;
             for (var i = 0; i < tracks.length; i++) {
 
                 var currentTitle = tracks[i].title;
                 currentTitle = normalize(currentTitle);
-                query = normalize(query);
                 //console.log(currentTitle);
                 // console.log(tracks[i].title);
 
                 var score = currentTitle.score(query);
+
                 var streamable = tracks[i].streamable;
                 var sharing = tracks[i].sharing;
                 //console.log(score);
                 //console.log(query + " " + currentTitle);
-                if (score > 0.3 && streamable == true && sharing == "public") {
+                if (score > stringScoreTolerance && streamable == true && sharing == "public") {
+                    count++;
                     tracks[i].score = score;
+                    //console.log(score);
                     properTracks.push(tracks[i]);
                 }
             }
+            console.log("count");
+            console.log(count);
             console.log(properTracks.length);
-            properTracks.sort(sortByScore);
 
 
         },
@@ -162,9 +166,9 @@ App.MainModel = (function () {
 
         iterateArray = function (array) {
             for (var i = 0; i < array.length; i++) {
-                console.log(array[i]);
+                //console.log(array[i]);
                 console.log(array[i].score);
-                console.log(array[i].favoritings_count);
+                console.log(array[i].playback_count);
                 console.log(array[i].permalink_url);
 
             }
