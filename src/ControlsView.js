04d0178ceb3557756f9c19fea02d273c8ccf8bdd
mopat <morczinietz@gmail.com>
2014-12-09 17:22:19 +0100

    footer sticky again

--- src/ControlsView.js
+++ src/ControlsView.js
@@ -42,21 +42,7 @@ App.ControlsView = (function () {
         },
 
         initStickyFooter = function () {
-            $footer = $('#sticky-footer');
-            $win = $(window);
-            var ipos = $footer.offset().top;
-            var wpos, space;
 
-            function h(e) {
-                wpos = $win.scrollTop();
-                space = $win.height() - $footer.height() * 2;
-                if (wpos + space < ipos) {
-                    $footer.addClass('fixed-footer');
-                } else {
-                    $footer.removeClass('fixed-footer');
-                }
-            }
-            $(window).ready(h).resize(h).scroll(h);
         },
 
 
