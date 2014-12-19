79dab053f7c25dffe5889f5f5d69f16056cf90af
mopat <morczinietz@gmail.com>
2014-12-19 20:16:56 +0100

    tabModes View Created

--- src/SearchView.js
+++ src/SearchView.js
@@ -4,16 +4,33 @@
 App.SearchView = (function () {
     var that = {},
         $searchField = null,
+        $yearSliderBox = null,
         $yearSlider = null,
         $searchButton = null,
         $picker = null,
-        $advancedSearchBox = null,
+        $hottestSwitchBox = null,
+        $similarSwitchBox = null,
+        $yearSwitchBox = null,
+        $newSwitchBox = null,
+        $hottestSwitch = null,
+        $similarSwitch = null,
+        $yearSwitch = null,
+        $newSwitch = null,
 
         init = function () {
             $searchField = $("#search-field");
             $searchButton = $("#search-button");
+            $yearSliderBox = $("#year-slider-box");
             $yearSlider = $("#year-slider");
             $picker = $(".picker");
+            $hottestSwitchBox = $("#hottest-switch-box");
+            $similarSwitchBox = $("#similar-switch-box");
+            $yearSwitchBox = $("#year-switch-box");
+            $newSwitchBox = $("#new-tracks-switch-box");
+            $hottestSwitch = $("#hottest-switch");
+            $similarSwitch = $("#similar-switch");
+            $yearSwitch = $("#year-switch");
+            $newSwitch = $("#new-tracks-switch");
 
             initYearSlider();
 
@@ -43,15 +60,55 @@ App.SearchView = (function () {
             $(that).trigger("searchButtonClicked", [$searchField.val(), $('#year-slider-value-lower').html(), $('#year-slider-value-upper').html(), $(".picked").attr("id")]);
         },
 
-        handleTabClicked = function(e){
+        handleTabClicked = function (e) {
             $(".picked").switchClass("picked", "unpicked", 0);
             $(e.target).closest(".picker").switchClass("unpicked", "picked");
+            var tabId = $(this).attr("id");
+            setMode(tabId);
         },
 
-        handleSwitchClicked = function(e){
+        handleSwitchClicked = function (e) {
             e.preventDefault();
             $(".checked").removeAttr("checked").removeClass("checked");
             $(e.target).closest("input").attr("checked", true).addClass("checked");
+        },
+
+        setMode = function (tabId) {
+            switch (tabId) {
+                case "artist-tab":
+                    artistMode();
+                    break;
+                case "track-tab":
+                    trackMode();
+                    break;
+                case "genre-tab":
+                    genreMode();
+                    break;
+            }
+        },
+
+        artistMode = function () {
+            $hottestSwitchBox.show();
+            $yearSliderBox.show();
+            $similarSwitchBox.show();
+            $newSwitchBox.show();
+            $yearSwitchBox.show();
+        },
+
+        trackMode = function () {
+            $hottestSwitchBox.hide();
+            $yearSliderBox.hide();
+            $newSwitchBox.hide();
+            $yearSwitchBox.hide();
+            $similarSwitchBox.show();
+        },
+
+        genreMode = function () {
+            $hottestSwitchBox.show();
+            $yearSliderBox.show();
+            $newSwitchBox.show();
+            $yearSwitchBox.show();
+            $similarSwitchBox.hide();
         };
 
 
