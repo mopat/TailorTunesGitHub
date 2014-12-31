4bf56f5a597461a56d672ed5c79bbcabcdbad0cd
mopat <morczinietz@gmail.com>
2014-12-31 14:18:59 +0100

    save and retrieve user data first step

--- src/PlaylistManager.js
+++ src/PlaylistManager.js
@@ -5,10 +5,17 @@ App.PlaylistManager = (function () {
     var that = {},
         APPLICATION_ID = "yOTWw2niwOWRTql2MtewglSVcXYQa36Bld6ztZX3",
         JAVASCRIPT_KEY = "wyt0MOGfNQxPCEC3fFDkxGmpukQ7ulbOzeMY27Ql",
+        savePlaylistButton = null,
 
         init = function(){
             Parse.initialize(APPLICATION_ID, JAVASCRIPT_KEY);
+savePlaylistButton = $("#save-playlist-button");
 
+            savePlaylistButton.on("click", savePlaylist);
+            logIn();
+        },
+
+        signIn = function(){
             var user = new Parse.User();
             user.set("username", "patrick");
             user.set("password", "killer");
@@ -26,17 +33,47 @@ App.PlaylistManager = (function () {
                     alert("Error: " + error.code + " " + error.message);
                 }
             });
+        },
 
-            var TestObject = Parse.Object.extend("TestObject");
-            var testObject = new TestObject();
-            testObject.save({foo: "bar"}, {
-                success: function(object) {
-
+        logIn = function(){
+            Parse.User.logIn("patrick", "killer", {
+                success: function(user) {
+                    console.log("LOGGEDIN")
                 },
-                error: function(model, error) {
-
+                error: function(user, error) {
+                    // The login failed. Check error to see why.
                 }
             });
+        },
+
+        loadPlaylists = function(){
+
+        },
+
+        savePlaylist = function(){
+            var user = Parse.User.current();
+
+// Make a new post
+            var Post = Parse.Object.extend("Playlists");
+            var post = new Post();
+            post.set("title", "playlist namesss");
+            post.set("JSONObject", "JSSSOOOOssssssssssON");
+            post.set("user", user);
+            post.save(null, {
+                success: function(post) {
+                    // Find all posts by the current user
+                    var query = new Parse.Query(Post);
+
+                    query.equalTo("user", user);
+                    query.find({
+                        success: function(usersPosts) {
+                            // userPosts contains all of the posts by the current user.
+                            console.log(usersPosts)
+                        }
+                    });
+                }
+            });
+
         };
 
     that.init = init;
