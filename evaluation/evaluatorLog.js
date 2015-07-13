310292fa08d3915ba2604a3b7cdc7c5368022312
mopat <morczinietz@gmail.com>
2015-07-13 15:34:41 +0200

    success faile page schöner

---
+++ evaluation/evaluatorLog.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Patrick on 06.07.2015.
+ */
+var isTaskRunning = false,
+    $evButton = $("#ev-button"),
+    $taskNumber = $("#task-number"),
+    $userNumber = $("#user-number"),
+    $taskSuccess = $("#task-success"),
+    $evHide = $(".ev-hide"),
+    $taskFail = $("#task-fail"),
+    $device = $("#device"),
+    filename = null,
+    groupIndicator = null;
+
+
+$taskSuccess.on("click", function (e) {
+
+    var datetime = Date.now(),
+        uid = $userNumber.val(),
+        task = $taskNumber.val(),
+        device = $device.val(),
+        eventType = e.type;
+    setGroupIndicator(device);
+    c = "success";
+    filename = "p" + uid + "_" + device;
+    var data = datetime + ";" + uid + ";" + task + ";" + groupIndicator + ";" + device + ";" + eventType + ";" + c + "\n";
+    stopLog(data, device, uid);
+});
+
+$taskFail.on("click", function (e) {
+    var datetime = Date.now(),
+        uid = $userNumber.val(),
+        task = $taskNumber.val(),
+        device = $device.val(),
+        eventType = e.type;
+    setGroupIndicator(device);
+    c = "fail";
+    filename = "p" + uid + "_" + device;
+    var data = datetime + ";" + uid + ";" + task + ";" + groupIndicator + ";" + device + ";" + eventType + ";" + c + "\n";
+    stopLog(data, device, uid);
+});
+
+function stopLog(data, device, uid) {
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:63342/TailorTunesGithub/evaluation/receiver.php',//url of receiver file on server
+        data: {data: data, directory: device, userId: uid, filename: filename}, //your data
+        success: function (datas) {
+            alert("Success")
+        }, //callback when ajax request finishes
+        dataType: "text" //text/json...
+    });
+}
+
+function setGroupIndicator(device) {
+    if (device == "Desktop")
+        groupIndicator = 1;
+    else if (device == "Mobile")
+        groupIndicator = 2;
+    else if (device == "Tabletop")
+        groupIndicator = 3;
+
+}
--- evaluation/log.js
+++ evaluation/log.js
@@ -6,9 +6,7 @@ var $evButton = $("#ev-button"),
     $evHide = $(".ev-hide"),
     $taskNumber = $("#task-number"),
     $userNumber = $("#user-number"),
-    $taskSuccess = $("#task-success");
-$taskFail = $("#task-fail");
-$device = $("#device"),
+    $device = $("#device"),
     evCount = 0,
     groupIndicator = null,
     filename = null,
@@ -18,69 +16,11 @@ $device = $("#device"),
     tStart = null,
     isTaskSucceeded = null;
 
-$taskSuccess.on("click", function () {
-    handleTaskSuccessOrFail(true);
-});
-$taskFail.on("click", function () {
-    handleTaskSuccessOrFail(false);
-});
-
-function handleTaskSuccessOrFail(isSucceeded) {
-    isTaskSucceeded = false;
-    $taskSuccess.hide();
-    $taskFail.hide();
-    $evButton.show();
-};
 $evButton.on("click", function (e) {
-    if (isTaskRunning) {
-        isTaskRunning = false;
-    }
-    else {
         isTaskRunning = true;
         lastDatetime = Date.now();
         tStart = Date.now();
-        $taskSuccess.show();
-        $taskFail.show();
-        $(this).hide();
-    }
-    if (isTaskRunning) {
-        $(this).html("Stop").css({left: 0, position: "absolute"});
         $evHide.hide();
-    }
-    else {
-
-        $(this).html("Start").css({left: "none", position: "relative"});
-        $evHide.show();
-        removeEventListener();
-        var c = "",
-            datetime = Date.now(),
-            uid = $userNumber.val(),
-            task = $taskNumber.val(),
-            device = $device.val(),
-            eventType = e.type;
-        c = "end";
-
-        createLog(datetime, uid, task, groupIndicator, device, eventType, c);
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:63342/TailorTunesGithub/evaluation/receiver.php',//url of receiver file on server
-            data: {data: data, directory: device, userId: uid, filename: filename, header: HEADER}, //your data
-            success: function (datas) {
-                alert("SUCCESS");
-                data = "";
-                evCount = 0;
-                c = "";
-                lastDatetime = null;
-                tStart = null;
-                isTaskSucceeded = null;
-                $taskSuccess.hide();
-                $taskFail.hide();
-            }, //callback when ajax request finishes
-            dataType: "text" //text/json...
-        });
-
-
-    }
 });
 
 
@@ -136,26 +76,37 @@ function log(e) {
 
         if (evCount == 0)
             c = "start";
-        else if (isTaskSucceeded)
-            c = "success";
-        else if (isTaskSucceeded == false)
-            c = "fail";
         else
             c = evCount;
         createLog(datetime, uid, task, groupIndicator, device, eventType, c)
     }
-
-
 }
 
 function createLog(datetime, uid, task, groupIndicator, device, eventType, c) {
     evCount++;
     var tDiff = datetime - lastDatetime;
     var tComplete = datetime - tStart;
-    data += datetime + ";" + uid + ";" + task + ";" + groupIndicator + ";" + device + ";" + eventType + ";" + c + ";" + tDiff + ";" + tComplete + "\n";
+    data = datetime + ";" + uid + ";" + task + ";" + groupIndicator + ";" + device + ";" + eventType + ";" + c + ";" + tDiff + ";" + tComplete + "\n";
     console.log(data);
     lastDatetime = datetime;
 
-    //console.log(data);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:63342/TailorTunesGithub/evaluation/receiver.php',//url of receiver file on server
+        data: {data: data, directory: device, userId: uid, filename: filename, header: HEADER}, //your data
+        success: function (datas) {
+            data = "";
+            evCount = 0;
+            c = "";
+            lastDatetime = null;
+            tStart = null;
+            isTaskSucceeded = null;
+        }, //callback when ajax request finishes
+        dataType: "text" //text/json...
+    });
+}
 
+function showEvHide() {
+    $evHide.show();
+    alert("SHOOW")
 }
